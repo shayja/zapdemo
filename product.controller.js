f7326f29d4084bdf3551ed6a00a8f60943d5ea7c
shayja <shayja@gmail.com>
2020-09-03 13:56:07 +0300

    added model with mondodb

--- product.controller.js
+++ product.controller.js
@@ -1,46 +1,131 @@
-exports.create = (req, res) => {
-    console.log('calling api method: create, params:', req.body);
+const db = require('./product.model');
 
-    if (!req.body.name){
-        res.status(404).json({message: 'חובה לשלוח את שם המוצר'});
-    } else {
-        res.status(200).json({ message: 'ok' });
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to the database!');
+    })
+    .catch(err => {
+        console.log('Cannot connect to the database!', err);
+        process.exit();
+    });
+
+
+const Product = db.Product;
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+        res.status(400).send({ message: 'Product name cannot be empty!' });
+        return;
     }
+
+    // Create a Product
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price ? req.body.price : 0
+    });
+
+    console.log('Product to save', product);
+
+    // Save Product in the database
+    product
+        .save(product)
+        .then(data => {
+            console.log('then(data', data);
+            res.send(data);
+        })
+        .catch(err => {
+            console.error('Products save err', err);
+
+            res.status(500).send({
+                message:
+                err.message || 'Some error occurred while creating the product.'
+            });
+        });
 };
 
 exports.findAll = (req, res) => {
-    console.log('calling api method: findAll');
-    const data =  [
-        {'id': 'abc', 'name': 'Galaxy S20', 'price': 4000}, 
-        {'id': 'def', 'name': 'Iphone X', 'price': 4500}
-    ];
-    res.status(200).json(data);
+    Product.find()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                message:
+                    err.message || 'Some error occurred while retrieving products.'
+            });
+        });
 };
 
 exports.findOne = (req, res) => {
     console.log(`calling api method: findOne, id: ${req.params.id}`);
-    const data =  [
-        {'id': 'abc', 'name': 'Galaxy S20', 'price': 4000}, 
-        {'id': 'def', 'name': 'Iphone X', 'price': 4500}
-    ];
-    const p = data.find(x=>x.id === req.params.id);
-    console.log(p);
-    res.status(200).json(p);
+    const id = req.params.id;
+
+    Product.findById(id)
+        .then(data => {
+            if (!data){
+                res.status(404).send({ message: `Product Not found # id ${id}` });
+            } else {
+                res.send(data);
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({ message: `Error retrieving Product with id=${id}` });
+        });
 };
 
 exports.update = (req, res) => {
-    console.log(`calling api method: update, id: ${req.params.id}`);
-
     if (!req.body) {
         return res.status(400).send({
-          message: "Data to update can not be empty!"
+            message: 'Data to update can not be empty!'
         });
-    } else {
-        res.status(200).json({ message: 'ok' });
     }
+
+    const id = req.params.id;
+
+    Product.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Product with id=${id} not found!` });
+            } else {
+                res.send({ message: 'Product was updated successfully.' });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                message: `Cannot update Product #${id}`
+            });
+        });
 };
 
+// Delete a Product with the specified id in the request
 exports.delete = (req, res) => {
-    console.log(`calling api method: delete, id: ${req.params.id}`);
-    res.status(200).json({ message: 'ok' });
+    const id = req.params.id;
+
+    Product.findByIdAndRemove(id, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Product with id=${id} not found!`
+                });
+            } else {
+                res.send({
+                    message: 'Product was deleted successfully!'
+                });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                message: `Cannot delete Product #${id}`
+            });
+        });
 };
--- product.model.js
+++ product.model.js
@@ -1,20 +1,30 @@
-module.exports = mongoose => {
-    var schema = mongoose.Schema(
-      {
-        name: String,
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = 'mongodb://localhost:27017/zapdemo_db';
+
+const schema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 2
+        },
         description: String,
         price: Number
-      },
-      { timestamps: true }
-    );
+    },
+    { timestamps: true }
+);
+
+schema.method('toJSON', function() {
+    const { _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
   
-    schema.method("toJSON", function() {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
-    });
-  
-    const product = mongoose.model("product", schema);
-    return Product;
-  };
-  
+db.Product = mongoose.model('Product', schema);
+
+module.exports = db;
