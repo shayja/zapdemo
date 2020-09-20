8a2bd13a57234ff9478e8fecb70d89c13baa8d30
Shay Jacoby <shayja@gmail.com>
2020-09-20 11:09:38 +0300

    added mockdata file

--- mockdata/product.router.js
+++ mockdata/product.router.js
@@ -1,42 +1,45 @@
-module.exports = (app) => {
+const router = require('express').Router();
 
-    const router = require('express').Router();
-  
-    // Create a new product
-    router.post('/', function(req, res){ 
-        res.status(200).json({ message: 'ok create'}); 
-    });
+// Create a new product
+router.post('/', function (req, res) {
+    console.log(req.body);
+    res.status(200).json('OK - product created');
+});
 
-    // Retrieve all products
-    router.get('/', function(req, res){ 
-        res.status(200).json({ message: 'ok findAll' }); 
-    });
+// Retrieve all products
+router.get('/', function (req, res) {
+    res.status(200).json('OK - product get all');
+});
 
-    // Retrieve a single product by its id
-    router.get('/:id', function(req, res){ 
-        res.status(200).json({ message: 'ok findOne' }); 
-    });
+// Retrieve a single product by its id
+router.get('/:id', function (req, res) {
+    console.log(req.params);
+    res.status(200).json('OK - product get by id');
+});
 
-    // Update a single product by its id
-    router.put('/:id', function(req, res){ 
-        res.status(200).json({ message: 'ok update' }); 
-    });
+// Update a single product by its id
+router.put('/:id', function (req, res) {
+    console.log(req.body);
+    res.status(200).json('OK - product updated');
+});
 
-    // Update some of the product properties.
-    router.patch('/:id', function(req, res){ 
-        res.status(200).json({ message: 'ok updateProps' }); 
-    });
+// Update some of the product properties.
+router.patch('/:id', function (req, res) {
+    console.log(req.body);
+    res.status(200).json('OK - product updated');
+});
 
-    // Delete a product with id
-    router.delete('/:id', function(req, res){ 
-        res.status(200).json({ message: 'ok delete' }); 
-    });
-  
-    // Search product by name
-    router.get('/search/:name', function(req, res){ 
-        res.status(200).json({ message: 'ok search' }); 
-    });
+// Delete a product with id
+router.delete('/:id', function (req, res) {
+    console.log(req.params);
+    res.status(200).json('OK - product deleted');
+});
 
-    app.use('/product', router);
+// Search product by name
+router.get('/search/:name', function (req, res) {
+    console.log(req.body.name);
+    res.status(200).json('OK - product search');
+});
 
-};
+
+module.exports = router;
---
+++ mockdata/server.js
@@ -0,0 +1,22 @@
+// console.log('Hello Zap');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is runing on port ${PORT}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to zap api' });
+});
+
+app.use('/product', require('./product.router'));
